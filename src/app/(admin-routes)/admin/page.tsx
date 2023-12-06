e91a12be9504224862d7ec03ca04870fff50c019
Paulo Cesar <paulo.santos@intelitrader.com.br>
2023-12-06 15:18:02 -0300

    add admin page, dashboard

---
+++ src/app/(admin-routes)/admin/page.tsx
@@ -0,0 +1,14 @@
+import Chat from "../../components/Chat";
+import MenuRooms from "../../components/MenuRooms";
+import { nextAuthOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+
+export default async function Dashboard() {
+  const session = await getServerSession(nextAuthOptions);
+  return (
+    <div className="flex h-full w-full">
+      <MenuRooms session={session} />
+      <Chat session={session} />
+    </div>
+  );
+}
