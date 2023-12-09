5900b41d5393165ec7b120458cb060630a8ff665
Paulo Cesar <paulo.santos@intelitrader.com.br>
2023-12-08 22:31:36 -0300

    edit route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -27,9 +27,17 @@ const nextAuthOptions: NextAuthOptions = {
         signIn: '/'
     },
     callbacks: {
-        async session({ session, token }) {
-            session = token.user as any
-            return session
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id;
+            }
+            return token;
+        },
+        async session({ session, token }: any) {
+            if (token) {
+                session.id = token.id;
+            }
+            return session;
         }
     }
 }
